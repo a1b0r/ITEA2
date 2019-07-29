e134d249fac2cce736d95a5edc8aa3f4c77ebcbe
Oleksandr Sydorchuk <a.sydorchuk@gmail.com>
2019-07-29 21:51:05 +0300

    task 02

--- lesson2/task2_shop.py
+++ lesson2/task2_shop.py
@@ -1,4 +1,8 @@
 class Shop:
+    _total_sales = 0
+
+
+class Market(Shop):
 
     _name = None
     _sales = 0
@@ -6,3 +10,36 @@ class Shop:
     def __init__(self, name, sales=0):
         self._name = name
         self._sales = sales
+        Shop._total_sales += self._sales
+
+    def add_sale(self,sale):
+        self._sales += sale
+        Shop._total_sales += self._sales
+
+    def __str__(self):
+        plural = ''
+        if self._sales > 1:
+            plural = 's'
+        return '{} sold {} item{}.'.format(self._name, self._sales, plural)
+
+    def print_sales(self):
+        print(self.__str__())
+
+
+atb1 = Market('ATB1', 1)
+atb1.add_sale(1)
+atb1.print_sales()
+
+atb2 = Market('ATB2', 2)
+atb2.add_sale(2)
+atb2.print_sales()
+
+atb3 = Market('ATB3', 3)
+atb3.add_sale(3)
+print(atb3)
+
+atb4 = Market('ATB4', 4)
+atb4.add_sale(4)
+print(atb4)
+
+print('Total sales: ' + str(Shop._total_sales))
