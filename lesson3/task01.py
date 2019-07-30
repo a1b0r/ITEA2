2c95db6b1d4023b2f46725eb692e5c854536e881
Oleksandr Sydorchuk <a.sydorchuk@gmail.com>
2019-07-30 20:08:32 +0300

    done

--- lesson3/task01.py
+++ lesson3/task01.py
@@ -4,7 +4,7 @@ class Dot:
     _y = 0
     _z = 0
 
-    def __int__(self, x=0, y=0, z=0):
+    def __init__(self, x=0, y=0, z=0):
         self._x = x
         self._y = y
         self._z = z
@@ -31,10 +31,31 @@ class Dot:
         return Dot(self.get_x() * other.get_x(), self.get_y() * other.get_y(), self.get_z() * other.get_z())
 
     def __add__(self, other):
-        return Dot(self.get_x() + other.get_x(), self.get_y() + other.get_y(), self.get_z() + other.get_z())
+        return self.get_x() + other.get_x(), self.get_y() + other.get_y(), self.get_z() + other.get_z()
+
+    def __sub__(self, other):
+        return self.get_x() - other.get_x(), self.get_y() - other.get_y(), self.get_z() - other.get_z()
+
+    def __neg__(self):
+        result = self.get_x() * -1, self.get_y() * -1, self.get_z() * -1
+        return result
+
+    def __str__(self):
+        return '({}, {}, {})'.format(self._x, self._y, self._z)
 
 
 dot1 = Dot(1, 2, 3)
 dot2 = Dot(1, 2, 3)
+print(dot1)
+print(dot2)
+print('-'*5)
+
+print(dot1 + dot2)
+print(dot1 - dot2)
+print('-'*5)
+
+print(-dot1)
+print('-'*5)
 
 dot3 = dot1 * dot2
+print(dot3)
