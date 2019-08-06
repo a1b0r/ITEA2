6d9983b4a3a010084f36dd776a67c1620b370a4d
Oleksandr Sydorchuk <a.sydorchuk@gmail.com>
2019-08-06 21:39:37 +0300

    done

--- lesson5/task01.py
+++ lesson5/task01.py
@@ -3,24 +3,28 @@ import random
 
 class File:
 
-    def __init__(self, filename, num_of_lines):
+    def __init__(self, filename, open_status):
+        print("__init__")
         self._filename = filename
-        self._num_of_lines = num_of_lines
+        self._open_status = open_status
 
     def __enter__(self):
-        print(f'Entered args are {self._filename, self._num_of_lines}')
-        return self
+        print("__enter__")
+        print(f'Entered args are {self._filename, self._open_status}')
+        self._f = open(self._filename, self._open_status)
+        return self._f
 
     def __exit__(self, *args):
-        print('The end of context manager')
-        print(args)
+        print("__exit__")
+        self._f.close()
+        if args[0] is None:
+            print(f'{args[0]}, {args[1]}, {args[2]}')
 
-    def open(self):
-        with open(self._filename, 'w') as f:
-            for l in range(self._num_of_lines):
-                f.write((str(random.randint(0, 5000)) + '\n'))
+    def __del__(self):
+        print("__del__")
 
 
-file = File('file01.txt',10)
-
-file.open()
+with File('file01.txt', 'w') as file:
+    print('__main__')
+    for i in range(random.randint(1, 10)):
+        file.write("This is line %d\r" % (i + 1))
