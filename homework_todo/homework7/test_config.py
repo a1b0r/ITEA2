fec0480ae910ae64495bb41907994b2d3d957fe2
a1b0r <a.sydorchuk@gmail.com>
2019-09-07 15:22:10 +0300

    homework_todo

--- homework_todo/homework7/test_config.py
+++ homework_todo/homework7/test_config.py
@@ -2,3 +2,8 @@ test_db = {"user": "root",
            "password": "",
            "host": "127.0.0.1",
            "database": "test"}
+
+university = {"user": "root",
+              "password": "",
+              "host": "127.0.0.1",
+              "database": "test"}
